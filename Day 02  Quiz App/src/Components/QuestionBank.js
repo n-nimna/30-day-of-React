983cae2404da6f44d2f319a94734a557efcc5696
Nimeshi Dilshara <120201164+n-nimna@users.noreply.github.com>
2024-07-30 03:36:58 +0530

    Add files via upload

---
+++ Day 02  Quiz App/src/Components/QuestionBank.js
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+    {
+        id: 1,
+        question: "What is the capital of Sri Lanka?",
+        options: ["Sri Jayawardenepura Kotte", "Galle", "Kandy", "Colombo"],
+        answer: "Sri Jayawardenepura Kotte",
+    },
+    {
+        id: 2,
+        question: "What is the capital of Korea?",
+        options: ["Busan", "Incheon", "Seoul", "Pyongyang"],
+        answer: "Seoul",
+    },
+    {
+        id: 3,
+        question: "What is the capital of India?",
+        options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
+        answer: "Delhi"
+    },
+    {
+        id: 4,
+        question: "What is the capital of America?",
+        options: ["New York City", "Washington, D.C", "Chicago", "Los Angeles"],
+        answer: "Washington, D.C."
+    },
+    {
+        id: 5,
+        question: "What is capital of China?",
+        options: ["Shanghai", "Hong Kong", "Beijing", "Guangzhou"],
+        answer: "Beijing"
+    },
+]
+
+export default qBank;
