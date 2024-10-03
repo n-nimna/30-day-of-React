ac063af14792f45ed611cda6a5d0131768e98d1b
Nimeshi Dilshara <120201164+n-nimna@users.noreply.github.com>
2024-10-03 13:11:06 +0530

    Add files via upload

---
+++ Day 29 Event-Countdown-Timer/App.js
@@ -0,0 +1,13 @@
+// src/App.js 
+import './App.css';
+import Countdown from './components/Countdown';
+
+function App() {
+  return (
+    <div className="App">
+      <Countdown />
+    </div>
+  );
+}
+
+export default App;
